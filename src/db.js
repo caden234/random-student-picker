9b3814ec305941c53fb74042ff9c410625b16beb
caden234 <cputthoff03@gmail.com>
2025-11-17 20:53:53 -0600

    Add random studnet functionalit

---
+++ src/db.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    id: 1,
+    name: "Alice Johnson",
+  },
+  {
+    id: 2,
+    name: "Bob Smith",
+  },
+  { id: 3, name: "Charlie Brown" },
+  {
+    id: 4,
+    name: "Diana Prince",
+  },
+  { id: 5, name: "Ethan Hunt" },
+];
--- src/main.js
+++ src/main.js
@@ -1,7 +1,37 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1 class="text-3xl font-bold underline">
-    Hello Vite!
-  </h1>
-`;
+import data from ".db.js";
+
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+const nameDisplay = document.querySelector("p");
+const darkModeRadio = document.getElementById("dark");
+const lightModeRadio = document.getElementById("light");
+
+button.addEventListener("click", () => {
+  nameDisplay.textContent =
+    data[
+      getRandomIntInclusive(
+        0,
+
+        // Subtract 1 to account for zero-based indexing
+        data.length - 1,
+      )
+    ].name;
+});
+
+darkModeRadio.addEventListener("change", () => {
+  body.classList.remove("bg-gray-100");
+  body.classList.add("bg-gray-800", "text-white");
+});
+
+lightModeRadio.addEventListener("change", () => {
+  body.classList.remove("bg-gray-800", "text-white");
+  body.classList.add("bg-gray-100");
+});
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1)) + minCeiled;
+}
